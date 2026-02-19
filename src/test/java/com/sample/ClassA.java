1911b2d9cc6e1b6d93b109b486773a33322d8db9
anusha-s05 <anushamol308@gmail.com>
2026-02-19 15:02:04 +0530

    new project - first commit

---
+++ src/test/java/com/sample/ClassA.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class ClassA {
+
+	public void mathod1() {
+		System.out.println("method 1");
+	}
+	
+	public void mathod2() {
+		System.out.println("method 2");
+	}
+	
+	public void mathod3() {
+		System.out.println("method 3");
+	}
+}
